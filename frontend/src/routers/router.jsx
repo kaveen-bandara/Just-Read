76b0a946b8b0574cbe4e79ea119a6c4065ef202f
Kaveen Bandara <kaveenbandara1018@gmail.com>
2024-12-23 19:09:57 +0530

    v0.3
    Login and Register done

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 import Home from '../pages/home/Home'
+import Login from '../components/Login'
+import Register from '../components/Register'
 
 const router = createBrowserRouter([
     {
@@ -8,16 +10,24 @@ const router = createBrowserRouter([
       element: <App />,
       children: [
         {
-            path: '/',
-            element: <Home />,
+          path: '/',
+          element: <Home />,
         },
         {
-            path: '/orders',
-            element: <div>Orders</div>
+          path: '/orders',
+          element: <div>Orders</div>,
         },
         {
-            path: '/about',
-            element: <div>About</div>
+          path: '/about',
+          element: <div>About</div>,
+        },
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          path: '/register',
+          element: <Register />,
         },
       ]
     },
